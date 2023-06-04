AOSPA-sweet/android_vendor_aospa

vendor: Add sweet
Change-Id: I6b0a6d12a7109b4f332659263c762c1817e587df

products/AndroidProducts.mk
@@ -17,11 +17,13 @@ PRODUCT_MAKEFILES += \
     $(LOCAL_DIR)/alioth/aospa_alioth.mk \
     $(LOCAL_DIR)/mini5g/aospa_mini5g.mk \
     $(LOCAL_DIR)/oneplus9/aospa_oneplus9.mk \
-    $(LOCAL_DIR)/oneplus9pro/aospa_oneplus9pro.mk
+    $(LOCAL_DIR)/oneplus9pro/aospa_oneplus9pro.mk \
+    $(LOCAL_DIR)/sweet/aospa_sweet.mk
 
 # Lunch targets
 COMMON_LUNCH_CHOICES += \
     aospa_alioth-userdebug \
     aospa_mini5g-userdebug \
     aospa_oneplus9-userdebug \
-    aospa_oneplus9pro-userdebug
+    aospa_oneplus9pro-userdebug \
+    aospa_sweet-userdebug

AOSPA-sweet/android_vendor_aospa1

products/AndroidProducts.mk
@@ -49,7 +49,8 @@ PRODUCT_MAKEFILES += \
     $(LOCAL_DIR)/spes/aospa_spes.mk \
     $(LOCAL_DIR)/sunny/aospa_sunny.mk \
     $(LOCAL_DIR)/surya/aospa_surya.mk \
-    $(LOCAL_DIR)/vayu/aospa_vayu.mk
+    $(LOCAL_DIR)/vayu/aospa_vayu.mk \
+    $(LOCAL_DIR)/sweet/aospa_sweet.mk
 
 # Lunch targets
 COMMON_LUNCH_CHOICES += \
@@ -88,4 +89,5 @@ COMMON_LUNCH_CHOICES += \
     aospa_spes-userdebug \
     aospa_sunny-userdebug \
     aospa_surya-userdebug \
-    aospa_vayu-userdebug
+    aospa_vayu-userdebug \
+    aospa_sweet-userdebug